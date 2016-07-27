2d2f2a778507088d7501080a4ac1f106d76a08b7
Douglas.Gaer@noaa.gov <Douglas.Gaer@noaa.gov>
2016-07-27 03:15:44 -0500

    updated CM makefile

--- cluster_manager/drcm_server/m_help.cpp
+++ cluster_manager/drcm_server/m_help.cpp
@@ -38,7 +38,7 @@ Help functions for Datareel cluster manager.
 #include "m_help.h"
 
 // Version number, program  and publisher info
-const char *VersionString = "1.14";
+const char *VersionString = "1.15";
 const char *ProgramDescription = "Datareel Cluster Manager";
 const char *ProducedBy = "http://datareel.com";
 
