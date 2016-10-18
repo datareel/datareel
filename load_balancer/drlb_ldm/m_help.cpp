0687cc29c2dc5ff423689a42218b1f2b5c1b3211
dgaer <Douglas.Gaer@noaa.gov>
2016-10-18 19:00:17 +0000

    DRLB LDM fixes for close_nointr call from safe_close ABRT signal Assertion EBADF

--- load_balancer/drlb_ldm/m_help.cpp
+++ load_balancer/drlb_ldm/m_help.cpp
@@ -6,7 +6,7 @@
 // C++ Compiler Used: GNU, Intel
 // Produced By: DataReel Software Development Team
 // File Creation Date: 06/17/2016
-// Date Last Modified: 10/12/2016
+// Date Last Modified: 10/18/2016
 // Copyright (c) 2016 DataReel Software Development
 // ----------------------------------------------------------- // 
 // ------------- Program Description and Details ------------- // 
@@ -38,7 +38,7 @@ Help functions for Datareel load balancer.
 #include "m_help.h"
 
 // Version number, program  and publisher info
-const char *VersionString = "1.58";
+const char *VersionString = "1.59";
 const char *ProgramDescription = "DR LDM Load Balancer";
 const char *ProducedBy = "http://datareel.com";
 
